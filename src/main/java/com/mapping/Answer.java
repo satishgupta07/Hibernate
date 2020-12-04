327a0b5d7be3fc176467cb3a7fa11120633bc085
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-04 18:08:22 +0530

    Bidirectional OneToOne Mapping
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/mapping/Answer.java
+++ src/main/java/com/mapping/Answer.java
@@ -3,6 +3,7 @@ package com.mapping;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Answer {
@@ -12,28 +13,43 @@ public class Answer {
 	private int ansId;
 	private String answer;
 	
+	@OneToOne(mappedBy = "answer")
+	private Question question;
+	
 	public Answer() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public Answer(int ansId, String answer) {
+
+	public Answer(int ansId, String answer, Question question) {
 		super();
 		this.ansId = ansId;
 		this.answer = answer;
+		this.question = question;
 	}
-	
+
 	public int getAnsId() {
 		return ansId;
 	}
+
 	public void setAnsId(int ansId) {
 		this.ansId = ansId;
 	}
+
 	public String getAnswer() {
 		return answer;
 	}
+
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
-	
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+		
 }
--- src/main/java/com/mapping/Client.java
+++ src/main/java/com/mapping/Client.java
@@ -12,22 +12,24 @@ public class Client {
 		 SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		 
 		 Question q1 = new Question();
-		 q1.setQuesId(101);
+		 q1.setQuesId(121);
 		 q1.setQuestion("What is Java ?");
 		 
 		 Answer a1 = new Answer();
-		 a1.setAnsId(201);
+		 a1.setAnsId(221);
 		 a1.setAnswer("Java is programming language");
+		 a1.setQuestion(q1);
 		 
 		 q1.setAnswer(a1);
 		 
 		 Question q2 = new Question();
-		 q2.setQuesId(102);
+		 q2.setQuesId(122);
 		 q2.setQuestion("What is Collection Framework ?");
 		 
 		 Answer a2 = new Answer();
-		 a2.setAnsId(202);
+		 a2.setAnsId(222);
 		 a2.setAnswer("API to work with objects in Java");
+		 a2.setQuestion(q2);
 		 
 		 q2.setAnswer(a2);
 		 
@@ -41,6 +43,12 @@ public class Client {
 		 session.save(a2);
 		 
 		 tx.commit();
+		 
+		 // fetching........
+		 Question newQ = (Question) session.get(Question.class,121);
+		 System.out.println(newQ.getQuestion());
+		 System.out.println(newQ.getAnswer().getAnswer());
+		 
 		 session.close();
 		 factory.close();
 	}

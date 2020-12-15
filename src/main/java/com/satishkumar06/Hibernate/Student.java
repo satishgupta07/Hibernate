ad9e9f05a8e3417114c626b481e434ad4f7b27b7
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-15 14:41:15 +0530

    Second Level Caching
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/satishkumar06/Hibernate/Student.java
+++ src/main/java/com/satishkumar06/Hibernate/Student.java
@@ -1,9 +1,15 @@
 package com.satishkumar06.Hibernate;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class Student {
 	
 	@Id
